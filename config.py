0158b92c1b9454221ccd27260478fd0e4e9b6cfe
Ben Johnson <bdjohnson79@gmail.com>
2022-03-06 19:22:56 -0600

    Test

---
+++ config.py
@@ -0,0 +1,6 @@
+def config():
+    return('This is a secret key.')
+
+def mongoconnect():
+    return('mongodb+srv://recipes:egnKV2oEq0uFBb93@bdjohns1.aql3j.mongodb.net/recipes?retryWrites=true&w=majority')
+    
